463df1edcd0fec133a425f8c3e6551fa985aa5e7
Oleg <oleg@denivip.ru>
2013-04-02 22:07:23 +0400

    added Key class

--- src/core/EntitySpriteSheet.js
+++ src/core/EntitySpriteSheet.js
@@ -3,8 +3,8 @@
  * @author Oleg Pimenov, https://github.com/fost
  *
  */
-/*jslint nomen: true, plusplus: true, vars: true */
-/*global flyjs, createjs*/
+/*jslint nomen: true, plusplus: true, vars: true, eqeq: true */
+/*global flyjs, createjs, extend*/
 this.flyjs = this.flyjs || {};
 
 (function () {
@@ -12,66 +12,83 @@ this.flyjs = this.flyjs || {};
 
     var EntitySpriteSheet = function () {};
 
-    var p = EntitySpriteSheet.prototype = new flyjs.Entity();
+    var proto = EntitySpriteSheet.prototype = new flyjs.Entity();
 
-    /**
-     *
-     * @type {createjs.Stage}
-     * @protected
-     */
-    p._stage = null;
+    extend(proto, {
+        /**
+         * @property _stage
+         * @type {createjs.Stage}
+         * @private
+         */
+        _stage: null,
 
-    /**
-     *
-     * @type {Object}
-     * @protected
-     */
-    p.bitmapAnimation = null;
+        /**
+         * @property _loadedAssets
+         * @type {Array}
+         * @private
+         */
+        _loadedAssets: [],
 
-    /**
-     * @protected
-     * @type {null}
-     */
-    p.sprite = null;
+        /**
+         * @property bitmapAnimation
+         * @type {Object}
+         * @protected
+         */
+        bitmapAnimation: null,
 
-    p._loadedAssets = [];
+        /**
+         * @property sprite
+         * @type {null}
+         * @protected
+         */
+        sprite: null,
 
-    p.initialize = function (stage, spriteSheetManifest) {
+        /**
+         * @method initialize
+         * @param stage
+         * @param spriteSheetManifest
+         */
+        initialize: function (stage, spriteSheetManifest) {
 
-        if (!stage) {
-            throw new flyjs.Exception("error in parameters", "Stage is Null");
+            if (!stage) {
+                throw new flyjs.Exception("error in parameters", "Stage is Null");
+            }
+
+            if (!spriteSheetManifest) {
+                throw new flyjs.Exception("error in parameters", "spriteSheetManifest is Null");
+            }
+
+            this._stage = stage;
+
+            this.spriteSheet = new createjs.SpriteSheet(spriteSheetManifest);
+            this.sprite = new createjs.BitmapAnimation(this.spriteSheet);
+
+            this.loadManifest([{src: spriteSheetManifest.images[0], id: "sprite"}]);
+        },
+
+        loadManifest: function (manifest) {
+            this.loader = new createjs.LoadQueue(false);
+            this.loader.onFileLoad = this.handleFileLoad.bind(this);
+            this.loader.onComplete = this.handleComplete.bind(this);
+            this.loader.loadManifest(manifest);
+        },
+
+        handleFileLoad: function (event) {
+            this._loadedAssets.push(event.item);
+        },
+
+        handleComplete: function () {
+            var item = this._loadedAssets[0];
+            var id = item.id;
+            var result = this.loader.getResult(id);
+
+            if (item.type == createjs.LoadQueue.IMAGE) {
+                var bmp = new createjs.Bitmap(result);
+            }
+
+            this._stage.addChild(this.sprite);
         }
-
-        this._stage = stage;
-
-        this.spriteSheet = new createjs.SpriteSheet(spriteSheetManifest);
-        this.sprite = new createjs.BitmapAnimation(this.spriteSheet);
-
-        this.loadManifest([{src: spriteSheetManifest.images[0], id: "sprite"}]);
-    };
-
-    p.loadManifest = function (manifest) {
-        this.loader = new createjs.LoadQueue(false);
-        this.loader.onFileLoad = this.handleFileLoad.bind(this);
-        this.loader.onComplete = this.handleComplete.bind(this);
-        this.loader.loadManifest(manifest);
-    };
-
-    p.handleFileLoad = function (event) {
-        this._loadedAssets.push(event.item);
-    };
-
-    p.handleComplete = function () {
-        var item = this._loadedAssets[0];
-        var id = item.id;
-        var result = this.loader.getResult(id);
-
-        if (item.type == createjs.LoadQueue.IMAGE) {
-            var bmp = new createjs.Bitmap(result);
-        }
-
-        this._stage.addChild(this.sprite);
-    };
+    });
 
     flyjs.EntitySpriteSheet = EntitySpriteSheet;
 }());
---
+++ src/utils/Key.js
@@ -0,0 +1,210 @@
+/**
+ *
+ * @author Oleg Pimenov, https://github.com/fost
+ *
+ */
+/*jslint nomen: true, plusplus: true, vars: true, eqeq: true */
+/*global flyjs, createjs, extend*/
+
+this.flyjs = flyjs || {};
+
+(function () {
+
+    'use strict';
+    /**
+     * A helper for common key codes
+     * @class Key
+     * @constructor
+     */
+    var Key = {
+        ANY: -1,
+
+        LEFT: 37,
+        UP: 38,
+        RIGHT: 39,
+        DOWN: 40,
+
+        ENTER: 13,
+        COMMAND: 15,
+        CONTROL: 17,
+        SPACE: 32,
+        SHIFT: 16,
+        BACKSPACE: 8,
+        CAPS_LOCK: 20,
+        DELETE: 46,
+        END: 35,
+        ESCAPE: 27,
+        HOME: 36,
+        INSERT: 45,
+        TAB: 9,
+        PAGE_DOWN: 34,
+        PAGE_UP: 33,
+        LEFT_SQUARE_BRACKET: 219,
+        RIGHT_SQUARE_BRACKET: 221,
+
+        A: 65,
+        B: 66,
+        C: 67,
+        D: 68,
+        E: 69,
+        F: 70,
+        G: 71,
+        H: 72,
+        I: 73,
+        J: 74,
+        K: 75,
+        L: 76,
+        M: 77,
+        N: 78,
+        O: 79,
+        P: 80,
+        Q: 81,
+        R: 82,
+        S: 83,
+        T: 84,
+        U: 85,
+        V: 86,
+        W: 87,
+        X: 88,
+        Y: 89,
+        Z: 90,
+
+        F1: 112,
+        F2: 113,
+        F3: 114,
+        F4: 115,
+        F5: 116,
+        F6: 117,
+        F7: 118,
+        F8: 119,
+        F9: 120,
+        F10: 121,
+        F11: 122,
+        F12: 123,
+        F13: 124,
+        F14: 125,
+        F15: 126,
+
+        DIGIT_0: 48,
+        DIGIT_1: 49,
+        DIGIT_2: 50,
+        DIGIT_3: 51,
+        DIGIT_4: 52,
+        DIGIT_5: 53,
+        DIGIT_6: 54,
+        DIGIT_7: 55,
+        DIGIT_8: 56,
+        DIGIT_9: 57,
+
+        NUMPAD_0: 96,
+        NUMPAD_1: 97,
+        NUMPAD_2: 98,
+        NUMPAD_3: 99,
+        NUMPAD_4: 100,
+        NUMPAD_5: 101,
+        NUMPAD_6: 102,
+        NUMPAD_7: 103,
+        NUMPAD_8: 104,
+        NUMPAD_9: 105,
+        NUMPAD_ADD: 107,
+        NUMPAD_DECIMAL: 110,
+        NUMPAD_DIVIDE: 111,
+        NUMPAD_ENTER: 108,
+        NUMPAD_MULTIPLY: 106,
+        NUMPAD_SUBTRACT: 109,
+        /**
+         *
+         * @param charInt {int}
+         * @returns {string}
+         */
+        getName: function (charInt) {
+            if (charInt >= Key.A && charInt <= Key.Z) {
+                return String.fromCharCode(charInt);
+            }
+            if (charInt >= Key.F1 && charInt <= Key.F15) {
+                return "F" + String(charInt - 111);
+            }
+            if (charInt >= 96 && charInt <= 105) {
+                return "NUMPAD " + String(charInt - 96);
+            }
+            switch (charInt) {
+                case Key.LEFT:
+                    return "LEFT";
+
+                case Key.UP:
+                    return "UP";
+
+                case Key.RIGHT:
+                    return "RIGHT";
+
+                case Key.DOWN:
+                    return "DOWN";
+
+                case Key.ENTER:
+                    return "ENTER";
+
+                case Key.CONTROL:
+                    return "CONTROL";
+
+                case Key.SPACE:
+                    return "SPACE";
+
+                case Key.SHIFT:
+                    return "SHIFT";
+
+                case Key.BACKSPACE:
+                    return "BACKSPACE";
+
+                case Key.CAPS_LOCK:
+                    return "CAPS LOCK";
+
+                case Key.DELETE:
+                    return "DELETE";
+
+                case Key.END:
+                    return "END";
+
+                case Key.ESCAPE:
+                    return "ESCAPE";
+
+                case Key.HOME:
+                    return "HOME";
+
+                case Key.INSERT:
+                    return "INSERT";
+
+                case Key.TAB:
+                    return "TAB";
+
+                case Key.PAGE_DOWN:
+                    return "PAGE DOWN";
+
+                case Key.PAGE_UP:
+                    return "PAGE UP";
+
+                case Key.NUMPAD_ADD:
+                    return "NUMPAD ADD";
+
+                case Key.NUMPAD_DECIMAL:
+                    return "NUMPAD DECIMAL";
+
+                case Key.NUMPAD_DIVIDE:
+                    return "NUMPAD DIVIDE";
+
+                case Key.NUMPAD_ENTER:
+                    return "NUMPAD ENTER";
+
+                case Key.NUMPAD_MULTIPLY:
+                    return "NUMPAD MULTIPLY";
+
+                case Key.NUMPAD_SUBTRACT:
+                    return "NUMPAD SUBTRACT";
+
+                default:
+                    return String.fromCharCode(charInt);
+            }
+        }
+    };
+
+    flyjs.Key = Key;
+}());
