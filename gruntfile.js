082990d26efc7abaa903f116a29ccf5cf229900f
Oleg <oleg@denivip.ru>
2013-05-22 18:44:21 +0400

    update build files

--- gruntfile.js
+++ gruntfile.js
@@ -1,33 +1,58 @@
 /*global module:false*/
 module.exports = function (grunt) {
     'use strict';
-
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
         meta: {
             version: '<%= pkg.version %>',
             srcPath: 'src/',
-            deployPath: 'deploy/',
+            deployPath: './',
             banner:
-                '// Fly.js \n' +
-                '// Distributed under MIT license\n'
+                ' Fly.js \n' +
+                '(c) 2013 Oleg Pimenov, \n' +
+                'Distributed under MIT license\n' +
+                'http://github.com/Forestcamp/Fly.js \n'
         },
-        concat: {
-            options: {
-                stripBanners: true
-            },
-            dist: {
-                src: ['<%= meta.srcPath %>core/*.js', '<%= meta.srcPath %>render/*.js'],
-                dest: '<%= meta.deployPath %>cocat.js'
-            }
+        lint_files: [
+            'Gruntfile.js',
+            'example/collide/entities/*.js',
+            'example/collide/*.js',
+            'example/invaders/*.js',
+            'example/hero/*.js',
+            'src/core/collision/*.js',
+            'src/core/*.js',
+            'src/render/*.js',
+            'src/utils/*.js',
+            'src/collection/*.js',
+            'src/texture/*.js'
+        ],
+        lint_globals: {
+            flyjs: true,
+            window: true,
+            createjs: true
         },
+
+        /*** Tasks ***/
         uglify: {
             options: {
-
+                banner: '/* <%= meta.banner %> */'
             },
-            dist: {
-                src: '<%= meta.deployPath %>cocat.js',
-                dest: '<%= meta.deployPath %>cocat.min.js'
+            compress_flyjs: {
+                files: [{
+                    src: [
+                        'src/core/**/*.js',
+                        'src/render/*.js',
+                        'src/utils/*.js',
+                        'src/collection/*.js',
+                        'src/texture/*.js',
+                        'src/vendor/stats.min.js',
+                        'src/vendor/createjs/**/*.js'
+                    ],
+                    dest: '<%= meta.deployPath %>flyjs.min.js'
+                }],
+                options: {
+                    report: 'min'
+                }
             }
         },
         yuidoc: {
@@ -47,60 +72,27 @@ module.exports = function (grunt) {
                 eqeq: true,
                 plusplus: true,
                 bitwise: true,
-                globals: {
-                    flyjs: true,
-                    window: true,
-                    createjs: true
-                }
+                globals: '<%= lint_globals %>'
             },
-            files: [
-                'Gruntfile.js',
-                'example/collide/entities/*.js',
-                'example/collide/*.js',
-                'example/invaders/*.js',
-                'example/hero/*.js',
-                'src/core/collision/*.js',
-                'src/core/*.js',
-                'src/render/*.js',
-                'src/utils/*.js',
-                'src/collection/*.js',
-                'src/texture/*.js'
-            ]
+            files: '<%= lint_files %>'
         },
         jshint: {
             options: {
                 eqeqeq: true,
                 eqnull: true,
-                globals: {
-                    flyjs: true,
-                    window: true,
-                    createjs: true
-                }
+                globals: '<%= lint_globals %>'
             },
-            uses_defaults: [
-                'Gruntfile.js',
-                'example/collide/entities/*.js',
-                'example/collide/*.js',
-                'example/invaders/*.js',
-                'example/hero/*.js',
-                'src/core/collision/*.js',
-                'src/core/*.js',
-                'src/render/*.js',
-                'src/utils/*.js',
-                'src/collection/*.js',
-                'src/texture/*.js'
-            ]
+            uses_defaults: '<%= lint_files %>'
         }
     });
 
     // These plugins provide necessary tasks.
     grunt.loadNpmTasks('grunt-contrib-yuidoc');
-    grunt.loadNpmTasks('grunt-contrib-concat');
     grunt.loadNpmTasks('grunt-contrib-uglify');
     grunt.loadNpmTasks('grunt-contrib-jshint');
     grunt.loadNpmTasks('grunt-jslint');
 
     grunt.registerTask('docs', ['yuidoc']);
-
+    grunt.registerTask('ugli', ['uglify']);
     grunt.registerTask('release', ['jslint', 'jshint']);
 };
